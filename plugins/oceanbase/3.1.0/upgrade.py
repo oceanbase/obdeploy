5632a6c86bf75e4daae374d1b64faf201177753d
frf12 <master@furongfeng.cn>
2021-06-28 10:29:15 +0800

    1.0.1

---
+++ plugins/oceanbase/3.1.0/upgrade.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+# OceanBase Deploy.
+# Copyright (C) 2021 OceanBase
+#
+# This file is part of OceanBase Deploy.
+#
+# OceanBase Deploy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OceanBase Deploy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OceanBase Deploy.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from __future__ import absolute_import, division, print_function
+
+
+
+
+def upgrade(plugin_context, stop_plugin, start_plugin, connect_plugin, display_plugin, *args, **kwargs):
+    components = plugin_context.components
+    clients = plugin_context.clients
+    cluster_config = plugin_context.cluster_config
+    cmd = plugin_context.cmd
+    options = plugin_context.options
+    stdio = plugin_context.stdio
+    
+    zones_servers = {}
+    for server in cluster_config.servers:
+        config = cluster_config.get_server_conf(server)
+        zone = config['zone']
+        if zone not in zones_servers:
+            zones_servers[zone] = []
+        zones_servers[zone].append(server)
+    
+    all_servers = cluster_config.servers
+    for zone in zones_servers:
+        cluster_config.servers = zones_servers[zone]
+        stdio.print('upgrade zone "%s"' % zone)
+        if not stop_plugin(components, clients, cluster_config, cmd, options, stdio, *args, **kwargs):
+            return 
+        if not start_plugin(components, clients, cluster_config, cmd, options, stdio, *args, **kwargs):
+            return 
+    
+    cluster_config.servers = all_servers
+    ret = connect_plugin(components, clients, cluster_config, cmd, options, stdio, *args, **kwargs)
+    if ret and display_plugin(components, clients, cluster_config, cmd, options, stdio, ret.get_return('cursor'), *args, **kwargs):
+        return plugin_context.return_true()
