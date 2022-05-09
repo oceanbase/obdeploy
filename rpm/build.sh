cd7d5af0f59403d314854c8c104882798a2b5495
frf12 <master@furongfeng.cn>
2022-05-09 10:51:48 +0800

    link oceanbase-ce

--- rpm/build.sh
+++ rpm/build.sh
@@ -133,10 +133,10 @@ function build()
     rm -fr /usr/obd /usr/bin/obd
     cp ./dist/obd /usr/bin/obd 
     cp -fr ./profile/* /etc/profile.d/
+    cd $BUILD_DIR/plugins && ln -s oceanbase oceanbase-ce && mv obproxy obproxy-ce
+    cd $BUILD_DIR/config_parser && ln -s oceanbase oceanbase-ce
     mv $BUILD_DIR /usr/obd
     rm -fr dist
-    cd $BUILD_DIR/plugins && ln -s oceanbase oceanbase-ce && mv obproxy obproxy-ce
-    cd $BUILD_DIR/config_parser && ln -s oceanbase oceanbase-ce 
     chmod +x /usr/bin/obd
     chmod -R 755 /usr/obd/*
     chown -R root:root /usr/obd/*
